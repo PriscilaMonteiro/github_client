8a95cb0a290b5594fb3b0029f920f8f28d5ba56a
priscila monteiro <pririmonteiro@me.com>
2022-03-17 17:30:41 -0700

    Created Http request in flutter with API call

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,20 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '9457edc9b9c2a8609ea7';
+const githubClientSecret = 'a9cd2e1b79548aeb6e4e309e891cc4fd6dca52b3';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:github/github.dart';
+import 'package:window_to_front/window_to_front.dart';
+
+import 'github_oauth_credentials.dart';
+import 'src/github_login.dart';
+import 'src/github_summary.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitHub Client',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(title: 'GitHub Client'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(
+      builder: (context, httpClient) {
+        WindowToFront.activate(); // and this.
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: GitHubSummary(
+            gitHub: _getGitHub(httpClient.credentials.accessToken),
+          ),
+        );
+      },
+      githubClientId: githubClientId,
+      githubClientSecret: githubClientSecret,
+      githubScopes: githubScopes,
+    );
+  }
+}
+
+GitHub _getGitHub(String accessToken) {
+  return GitHub(auth: Authentication.withToken(accessToken));
+}
